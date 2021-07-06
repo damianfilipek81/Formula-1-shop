ad0a933dc6515bb85826fc5fe9d8a801a63a5ab3
Damian <damianfilipek81@gmail.com>
2021-07-06 11:14:58 +0200

    Add needed extensions and packages

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import "slick-carousel/slick/slick.css"; 
+import { Provider } from 'react-redux'
+import { store } from './redux/store';
+
+import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import './styles/settings.scss';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>,
   </React.StrictMode>,
   document.getElementById('root')
 );
