262d367185e5dd928b710430cd588a8120fdb93a
Damian <damianfilipek81@gmail.com>
2021-07-09 20:03:06 +0200

    Issue solved

--- backend/routes/products.routes.js
+++ backend/routes/products.routes.js
@@ -1,13 +1,10 @@
 const express = require('express');
 const Product = require('../models/Product.model');
-const path = require('path');
 
 const router = express.Router();
-const pathToImages = path.join(__dirname, '../../src/images/');
 
 
 router.get('/products', async (req, res) => {
-  console.log(req.session)
   try {
     const result = await Product.find();
     if (!result) res.status(404).json({ post: 'Not found' });
@@ -21,12 +18,9 @@ router.get('/products', async (req, res) => {
 router.get('/products/:id', async (req, res) => {
   try {
     const result = await Product.findById(req.params.id);
-    const output = {
-      ...result._doc,
-      path: pathToImages
-    };
+
     if (!result) res.status(404).json({ post: 'Not found' });
-    else res.json(output);
+    else res.json(result);
   }
   catch (err) {
     res.status(500).json(err);
